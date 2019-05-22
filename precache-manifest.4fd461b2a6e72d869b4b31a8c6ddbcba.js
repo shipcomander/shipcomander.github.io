bb5b5de40750fac24f77592fe1d3e2cc7ba9f750
RKN01011 <46045741+RKN01011@users.noreply.github.com>
2019-05-22 23:37:57 +0300

    game
    game

---
+++ precache-manifest.4fd461b2a6e72d869b4b31a8c6ddbcba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "20b8d7faeb738584e6e8",
+    "url": "/static/css/main.4894c9d6.chunk.css"
+  },
+  {
+    "revision": "20b8d7faeb738584e6e8",
+    "url": "/static/js/main.82dc5305.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "98b4a19b3a204133da2f",
+    "url": "/static/js/2.26e1a411.chunk.js"
+  },
+  {
+    "revision": "d704a706a684e47faceccdd5d2a1ff25",
+    "url": "/static/media/cross.d704a706.svg"
+  },
+  {
+    "revision": "e8b01be473556cdca871007b9bae8dc1",
+    "url": "/static/media/exp.e8b01be4.svg"
+  },
+  {
+    "revision": "b28ebc75558eb8110a21a628acfe12df",
+    "url": "/index.html"
+  }
+];
